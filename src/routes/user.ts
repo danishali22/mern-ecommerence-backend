325caff9c3e5db8af94db304af8487cac54e52c5
Danish Ali <dali27363@gmail.com>
2024-10-06 14:22:15 +0500

    create middleware for admin authorization

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import {deleteUser, getAllUsers, getUser, newUser, updateUser} from "../controllers/user.js";
+import { adminOnly } from "../middlewares/auth.js";
 
 const app = express.Router();
 
 app.post("/new", newUser);
 
-app.get("/all", getAllUsers);
+app.get("/all", adminOnly, getAllUsers);
 
-app.route("/:id").get(getUser).delete(deleteUser);
+app.route("/:id").get(getUser).delete(adminOnly, deleteUser);
 
 export default app;
