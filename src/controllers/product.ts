6ed02548f535c700ab9eac156d7d6ea896bc43d2
Danish Ali <dali27363@gmail.com>
2024-10-08 23:38:17 +0500

    "Refactored product controller, added invalidate cache feature, and updated types"

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -10,6 +10,71 @@ import ErrorHandler from "../utils/utitlity-class.js";
 import { rm } from "fs";
 import { faker } from "@faker-js/faker";
 import { myCache } from "../app.js";
+import { invalidateCache } from "../utils/features.js";
+
+export const latestProducts = TryCatch(async (req, res, next) => {
+  let products;
+
+  if (myCache.has("latest-products")) {
+    products = JSON.parse(myCache.get("latest-products")!);
+  } else {
+    products = await Product.find({}).sort({ created_at: -1 }).limit(5);
+    myCache.set("latest-products", JSON.stringify(products));
+  }
+
+  return res.status(200).json({
+    success: true,
+    data: products,
+  });
+});
+
+export const getAllCategories = TryCatch(async (req, res, next) => {
+  let products;
+  if (myCache.has("categories")) {
+    products = JSON.parse(myCache.get("categories")!);
+  } else {
+    products = await Product.distinct("category");
+    myCache.set("categories", JSON.stringify(products));
+  }
+
+  return res.status(200).json({
+    success: true,
+    data: products,
+  });
+});
+
+export const getAdminProducts = TryCatch(async (req, res, next) => {
+  let products;
+  if (myCache.has("admin-products")) {
+    products = JSON.parse(myCache.get("admin-products")!);
+  } else {
+    products = await Product.find({});
+    myCache.set("admin-products", JSON.stringify(products));
+  }
+
+  return res.status(200).json({
+    success: true,
+    data: products,
+  });
+});
+
+export const getProduct = TryCatch(async (req, res, next) => {
+  const id = req.params.id;
+  let product;
+  if (myCache.has(`product-${id}`)) {
+    product = JSON.parse(myCache.get(`product-${id}`)!);
+  } else {
+    product = await Product.findById(id);
+    if (!product) return next(new ErrorHandler("Product Not Found", 400));
+    myCache.set(`product-${id}`, JSON.stringify(product));
+  }
+
+  res.status(200).json({
+    success: true,
+    data: product,
+    message: "Product found successfully",
+  });
+});
 
 export const newProduct = TryCatch(
   async (
@@ -36,79 +101,14 @@ export const newProduct = TryCatch(
       photo: photo?.path,
     });
 
+    await invalidateCache({ product: true });
+
     res.status(201).json({
       success: true,
       message: "Product created successfully",
     });
   }
 );
-
-export const latestProducts = TryCatch(async (req, res, next) => {
-  let products;
-
-  if(myCache.has("latest-products")){
-    products = JSON.parse(myCache.get("latest-products")!);
-  } 
-  else{
-    products = await Product.find({}).sort({ created_at: -1 }).limit(5);
-    myCache.set("latest-products", JSON.stringify(products));
-  }
-
-
-
-  return res.status(200).json({
-    success: true,
-    data: products,
-  });
-});
-
-export const getAllCategories = TryCatch(async (req, res, next) => {
-  let products;
-  if(myCache.has("all-products")){
-    products = JSON.parse(myCache.get("all-products")!);
-  }
-  else{
-    products = await Product.distinct("category");
-    myCache.set("all-products", JSON.stringify(products))
-  }
-  return res.status(200).json({
-    success: true,
-    data: products,
-  });
-});
-
-export const getAdminProducts = TryCatch(async (req, res, next) => {
-  let products;
-  if(myCache.has("admin-products")){
-    products = JSON.parse(myCache.get("admin-products")!);
-  }
-  else{
-    products = await Product.find({});
-    myCache.set("admin-products", JSON.stringify(products))
-  }
-  return res.status(200).json({
-    success: true,
-    data: products,
-  });
-});
-
-export const getProduct = TryCatch(async (req, res, next) => {
-  const id = req.params.id;
-  let product;
-  if(myCache.has(`product-${id}`)){
-    product = JSON.parse(myCache.get(`product-${id}`)!);
-  }
-  else{
-    product = await Product.findById(id);
-    if (!product) return next(new ErrorHandler("Product Not Found", 400));
-    myCache.set(`product-${id}`, JSON.stringify(product))
-  }
-  res.status(200).json({
-    success: true,
-    data: product,
-    message: "Product found successfully",
-  });
-});
 
 export const updateProduct = TryCatch(async (req, res, next) => {
   const { id } = req.params;
@@ -131,6 +131,8 @@ export const updateProduct = TryCatch(async (req, res, next) => {
 
   const updatedProduct = await product.save();
 
+  await invalidateCache({ product: true });
+
   return res.status(201).json({
     success: true,
     data: updatedProduct,
@@ -146,6 +148,7 @@ export const deleteProduct = TryCatch(async (req, res, next) => {
     console.log("Product photo deleted");
   });
   await Product.deleteOne();
+  await invalidateCache({ product: true });
   return res.status(200).json({
     success: true,
     message: "Product deleted successfully",
@@ -159,7 +162,7 @@ export const searchFilterProducts = TryCatch(
     next: NextFunction
   ) => {
     const { search, category, price, sort } = req.query as SearchRequestQuery;
-    
+
     const page = Number(req.query.page) || 1;
     const limit = Number(process.env.PRODUCT_PER_PAGE) || 8;
     const skip = (page - 1) * limit;
--- src/utils/features.ts
+++ src/utils/features.ts
@@ -1,4 +1,7 @@
 import mongoose from "mongoose";
+import { InvalidateCacheProps } from "../types/types.js";
+import { Product } from "../models/product.js";
+import { myCache } from "../app.js";
 
 export const connectDB = () => {
     mongoose.connect("mongodb://localhost:27017", {  // Corrected connection string 127.0.0.1:27017
@@ -7,6 +10,29 @@ export const connectDB = () => {
     .then(() => console.log("DB connected successfully"))  // Log success
     .catch((error) => console.log(`DB connection error: ${error.message}`));  // Log error
 };
+
+export const invalidateCache = async ({product, order, admin}: InvalidateCacheProps) => {
+    if(product){
+        const productKeys: string[] = [
+            "latest-products",
+            "admin-products",
+            "categories",
+        ]
+
+        let products = await Product.find({}).select("_id");
+        products.forEach((i) => {
+            productKeys.push(`product-${i._id}`);
+        });
+
+        myCache.del(productKeys);
+    }
+    if(order){
+
+    }
+    if(admin){
+
+    }
+}
 
 
 /*
