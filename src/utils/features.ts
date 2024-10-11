0de2d83b1cb9182639e7696b404a684cdb10af91
Danish Ali <dali27363@gmail.com>
2024-10-12 00:57:57 +0500

    "Updated calculatePercentage function to use division instead of subtraction in calculation"

--- src/utils/features.ts
+++ src/utils/features.ts
@@ -65,7 +65,7 @@ export const reduceStock = async(orderItems: OrderItemsType[]) => {
 
 export const calculatePercentage = (thisMonth: number, lastMonth: number) => {
     if(lastMonth === 0) return thisMonth*100;
-    const percent =  ((thisMonth - lastMonth) / lastMonth) * 100;
+    const percent =  (thisMonth / lastMonth) * 100;
     return Number(percent.toFixed(0));
 }
 
